2382cc0eec48bf56d2162ee5e7ea7f6292660d62
Frank Jung <frankhjung@linux.com>
2013-06-04 21:48:28 +1000

    simplify script

--- loadxml.py
+++ loadxml.py
@@ -15,45 +15,40 @@ def main(argv):
             usage='%(prog)s [options]', 
             description='a Python example program to show XML processing',
             epilog='(c) 2013 Frank H Jung')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.0.1')
-    parser.add_argument('-v', '--verbose', help='verbose output', action='count')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default='test.xml', help='alternate XML file to test')
+    parser.add_argument('-v', '--verbose', help='verbose output', action='count')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.0.1')
     
     # process command line arguments
     args = parser.parse_args()
     infile = args.infile
     verbose = args.verbose
 
-    # show command parameters
-    example = 0
-    if verbose:
-        example += 1
-        print "\n(%s) dump XML file" % (example)
-        print "\tverbose = %s" % (verbose)
-        print "\tinfile = %s" % (infile.name)
-        print "\tdata:\n%s" % (infile.read())
-
     # load XML document from file
     xmldoc = xml.etree.ElementTree.parse(infile.name)
 
-    # dump XML document
-    if verbose:
-        example += 1
-        print "\n(%s) dump the XML that was read" % (example)
-        xml.etree.ElementTree.dump(xmldoc)
-
     # show turnover for a specific year
-    example += 1
-    print "\n(%s) show value for a specific year." % (example)
+    print "\n(1) show value for a specific year."
     turnover = getYear(xmldoc, '2012')
     print "\tYear 2012 has turnover of %s" % (turnover)
 
     # show turnover for all years
-    example += 1
-    print "\n(%s) show turnover for all years." % (example)
+    print "\n(2) show turnover for all years."
     turnovers = getAllYears(xmldoc)
     for t in turnovers:
         print "\tYear %s has turnover of %s" % (t.attrib['id'], t.text)
+
+    # show command parameters
+    if verbose:
+        print "\n(3) dump XML file"
+        print "\tverbose = %s" % (verbose)
+        print "\tinfile = %s" % (infile.name)
+        print "\tdata:\n%s" % (infile.read())
+
+    # dump XML document
+    if verbose:
+        print "\n(4) dump the XML that was read"
+        xml.etree.ElementTree.dump(xmldoc)
 
 def getYear(doc, year):
     """ Show turnover for a specific year. """
